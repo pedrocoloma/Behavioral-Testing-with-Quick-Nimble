ae5591539e5a428ca05d71e014bcf8f8ed7b3e89
Pedro <pedrocolomaa@gmaail.com>
2020-04-01 10:20:28 -0300

    Ädd tests and fix code

--- AppTacToeTests/BoardSpec.swift
+++ AppTacToeTests/BoardSpec.swift
@@ -32,6 +32,90 @@ import Nimble
 
 class BoardSpec: QuickSpec {
   override func spec() {
-
+    var board: Board!
+    
+    beforeEach {
+        board = Board()
+    }
+    
+    describe("playing") {
+        context("a single move") {
+            it("should switch to nought") {
+                try! board.playRandom()
+                expect(board.state).to(equal(.playing(.nought)))
+            }
+        }
+        
+        context("two moves") { // 1
+            it("should switch back to cross") {
+                try! board.playRandom() // 2
+                try! board.playRandom()
+                expect(board.state) == .playing(.cross) // 3
+            }
+        }
+        
+        context("a winning move") {
+            it("should switch to won state") {
+                // Arrange
+                try! board.play(at: 0)
+                try! board.play(at: 1)
+                try! board.play(at: 3)
+                try! board.play(at: 2)
+                
+                // Act
+                try! board.play(at: 6)
+                
+                // Assert
+                expect(board).to(beWon(by: .cross))
+            }
+        }
+        
+        context("a move leaving no remaining moves") {
+            it("should switch to draw state") {
+                // Arrange
+                try! board.play(at: 0)
+                try! board.play(at: 2)
+                try! board.play(at: 1)
+                try! board.play(at: 3)
+                try! board.play(at: 4)
+                try! board.play(at: 8)
+                try! board.play(at: 6)
+                try! board.play(at: 7)
+                
+                // Act
+                try! board.play(at: 5)
+                
+                // Assert
+                expect(board.state) == Board.State.draw
+            }
+        }
+        
+        context("a move that was already played") {
+            it("should throw an error") {
+                try! board.play(at: 0) // 1
+                
+                // 2
+                expect { try board.play(at: 0) }
+                    .to(throwError(Board.PlayError.alreadyPlayed))
+            }
+        }
+        
+        context("a move while the game was already won") {
+            it("should throw an error") {
+                // Arrange
+                try! board.play(at: 0)
+                try! board.play(at: 1)
+                try! board.play(at: 3)
+                try! board.play(at: 2)
+                try! board.play(at: 6)
+                
+                // Act & Assert
+                expect { try board.play(at: 7) }
+                    .to(throwError(Board.PlayError.noGame))
+            }
+        }
+        
+        
+    }
   }
 }
---
+++ README.md
@@ -0,0 +1,3 @@
+# Practicing Behavioral Testing
+
+App developed on Xcode 9.4 as part of a RayWenderlich tutorial to implement Beahvioral Testing using Quick Nimble.
